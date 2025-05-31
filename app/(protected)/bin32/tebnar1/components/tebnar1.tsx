9225be304479cb625d99a24e890bd9c623d17132
transatlanticvoyage <sandbagexpert@gmail.com>
2025-05-31 17:37:25 -0400

    sdf

---
+++ app/(protected)/bin32/tebnar1/components/tebnar1.tsx
@@ -0,0 +1,3 @@
+export default function Tebnar1() {
+  return <div></div>;
+} 
---
+++ app/(protected)/bin32/tebnar1/page.tsx
@@ -0,0 +1,9 @@
+import Tebnar1 from './components/tebnar1';
+
+export default function Tebnar1Page() {
+  return (
+    <main className="min-h-screen">
+      <Tebnar1 />
+    </main>
+  );
+} 
