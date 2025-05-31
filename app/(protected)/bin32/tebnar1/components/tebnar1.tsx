1983c741cdad9189b6a56925a440455019d00ad1
transatlanticvoyage <sandbagexpert@gmail.com>
2025-05-31 18:06:34 -0400

    fsd

--- app/(protected)/bin32/tebnar1/components/tebnar1.tsx
+++ app/(protected)/bin32/tebnar1/components/tebnar1.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from 'react';
 import { useAuth } from '@/app/context/AuthContext';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
