a7f3f1c07f1dcad1e350b217ef2314ad219c2a86
transatlanticvoyage <sandbagexpert@gmail.com>
2025-06-01 00:32:37 -0400

    sdf

--- app/(protected)/bin32/tebnar1/components/tebnar1.tsx
+++ app/(protected)/bin32/tebnar1/components/tebnar1.tsx
@@ -5,6 +5,10 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 // Table columns based on schema in setup-database/route.ts
 const columns = [
+  'fk_image1_id',
+  'fk_image2_id',
+  'fk_image3_id',
+  'fk_image4_id',
   'id',
   'rel_users_id',
   'rel_images_plans_batches_id',
@@ -17,10 +21,6 @@ const columns = [
   'e_more_instructions1',
   'e_prompt1',
   'e_ai_tool1',
-  'fk_image1_id',
-  'fk_image2_id',
-  'fk_image3_id',
-  'fk_image4_id',
 ];
 
 const gridCols = 9; // A-I
