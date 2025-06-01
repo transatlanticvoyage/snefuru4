a4a0af0798b0c3fdd67b10f5b35857a543d3ce57
transatlanticvoyage <sandbagexpert@gmail.com>
2025-05-31 21:16:30 -0400

    dsf

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -12,7 +12,7 @@ export default function ProtectedLayout({
     <ProtectedRoute>
       <div className="min-h-screen bg-gray-50">
         <Header />
-        <div className="w-full h-10 bg-black z-30 flex items-center px-4 text-white space-x-4">
+        <div className="sticky top-0 w-full h-10 bg-black z-30 flex items-center px-4 text-white space-x-4">
           <span>rel_images_plans_batches_id</span>
           <input
             type="text"
