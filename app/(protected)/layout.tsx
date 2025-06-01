5ceb29bd1661460f44af519bf57df828044ca682
transatlanticvoyage <sandbagexpert@gmail.com>
2025-05-31 21:32:03 -0400

    dfs

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -12,14 +12,6 @@ export default function ProtectedLayout({
     <ProtectedRoute>
       <div className="min-h-screen bg-gray-50">
         <Header />
-        <div className="sticky top-0 w-full h-10 bg-black z-30 flex items-center px-4 text-white space-x-4">
-          <span>rel_images_plans_batches_id</span>
-          <input
-            type="text"
-            className="h-7 px-2 rounded text-black"
-            style={{ minWidth: 120 }}
-          />
-        </div>
         <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {children}
         </main>
