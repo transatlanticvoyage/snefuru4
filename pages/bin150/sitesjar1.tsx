ee81d5fe2f69f4868a5679183be982888fd40eca
transatlanticvoyage <sandbagexpert@gmail.com>
2025-06-10 16:12:55 -0400

    dfs

--- pages/bin150/sitesjar1.tsx
+++ pages/bin150/sitesjar1.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '@/app/components/Header';
+import { AuthProvider } from '@/app/context/AuthContext';
 
 const SitesJar1 = () => {
     return (
-        <div>
-            <Header />
-            {/* Additional content will be added here later */}
-        </div>
+        <AuthProvider>
+            <div>
+                <Header />
+                {/* Additional content will be added here later */}
+            </div>
+        </AuthProvider>
     );
 };
 
