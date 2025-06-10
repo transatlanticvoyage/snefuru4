d58d4ad651f7bc789705fab9b750f74c22757aee
transatlanticvoyage <sandbagexpert@gmail.com>
2025-06-10 16:22:52 -0400

    dfs

--- pages/bin150/sitesjar1.tsx
+++ pages/bin150/sitesjar1.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '@/app/components/Header';
 import { AuthProvider } from '@/app/context/AuthContext';
+import ProtectedRoute from '@/app/components/ProtectedRoute';
 
 const SitesJar1 = () => {
+    useEffect(() => {
+        // Set document title
+        document.title = 'sitesjar1 - Snefuru';
+    }, []);
+
     return (
         <AuthProvider>
-            <div>
-                <Header />
-                {/* Additional content will be added here later */}
-            </div>
+            <ProtectedRoute>
+                <div className="min-h-screen bg-gray-50">
+                    <Header />
+                    <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                        {/* Additional content will be added here later */}
+                    </main>
+                </div>
+            </ProtectedRoute>
         </AuthProvider>
     );
 };
