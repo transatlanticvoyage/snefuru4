182e93c1b50d04af55151b24644da5a1c9037cec
transatlanticvoyage <sandbagexpert@gmail.com>
2025-06-23 12:54:16 -0400

    f

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -20,12 +20,18 @@ export default function ProtectedLayout({
   return (
     <ProtectedRoute>
       <div className="min-h-screen bg-gray-50">
-        <Header />
+        <div 
+          className={`transition-transform duration-300 ease-in-out ${
+            sidebarOpen ? 'transform translate-x-64' : ''
+          }`}
+        >
+          <Header />
+          <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            {children}
+          </main>
+        </div>
         <SidebarMenu isOpen={sidebarOpen} onToggle={toggleSidebar} />
         <SelectedRowStyles />
-        <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {children}
-        </main>
       </div>
     </ProtectedRoute>
   );
