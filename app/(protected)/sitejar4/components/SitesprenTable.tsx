c0d4a04e97b19fc3202b4e51fc1d99784d023096
transatlanticvoyage <sandbagexpert@gmail.com>
2025-06-26 11:15:28 -0400

    hy

--- app/(protected)/sitejar4/components/SitesprenTable.tsx
+++ app/(protected)/sitejar4/components/SitesprenTable.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import Link from 'next/link';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
