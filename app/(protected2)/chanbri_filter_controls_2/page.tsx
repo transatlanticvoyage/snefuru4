e33168c3d585ee3c394bed31c7a85c4ae9a823ba
transatlanticvoyage <sandbagexpert@gmail.com>
2025-07-07 17:14:24 -0400

    dsf

--- app/(protected2)/chanbri_filter_controls_2/page.tsx
+++ app/(protected2)/chanbri_filter_controls_2/page.tsx
@@ -8,6 +8,16 @@ export default function ChanbriFilterControls2Page() {
   return (
     <>
       <h1>Blank Page Here</h1>
+
+<div style={{ border: '1px solid #000', height: '200px', width: '100%' }}>
+  stuff
+</div>
+
+
+
+
+
+
     </>
   )
 }
