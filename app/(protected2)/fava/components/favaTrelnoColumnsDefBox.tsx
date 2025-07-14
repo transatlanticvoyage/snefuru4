931cf5ccf8d2f95bfdebd559d570341fe2b3922a
transatlanticvoyage <sandbagexpert@gmail.com>
2025-07-13 22:33:19 -0400

    sdf

---
+++ app/(protected2)/fava/components/favaTrelnoColumnsDefBox.tsx
@@ -0,0 +1,23 @@
+export default function FavaTrelnoColumnsDefBox() {
+  const boxStyle = {
+    border: '1px solid #000'
+  };
+
+  return (
+    <div className="biri_dev_info_box" style={boxStyle}>
+      <strong>biri_dev_info_box</strong><br></br>
+      xpages.xpage_id INSERT<br></br>
+      xpages.main_url INSERT<br></br>
+      xpages.meta_title INSERT<br></br>
+      xpages.main_url INSERT<br></br>
+      xpages.main_url INSERT<br></br>
+      LINK TO /obi W XPAGE SELECTED<br></br>
+      ========================<br></br>
+      xpages.view_info<br></br>
+      xapges.column count qty<br></br>
+      columns definition w vscode link<br></br>
+      -- link to /coltempcommand --<br></br>
+      -- link to /denbujar --<br></br>
+    </div>
+  );
+}
