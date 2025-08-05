7483b7a07b19d2f2a6b61d2b3a84996ec69dae5c
transatlanticvoyage <sandbagexpert@gmail.com>
2025-08-04 22:57:14 -0400

    dsf

---
+++ app/(protected)/cgigjar/page.tsx
@@ -0,0 +1,7 @@
+export default function CgigjarPage() {
+  return (
+    <div className="flex-1 p-6">
+      {/* Main content area - currently blank as requested */}
+    </div>
+  );
+}
---
+++ app/(protected)/driggsman/page.tsx
@@ -0,0 +1,7 @@
+export default function DriggsmanPage() {
+  return (
+    <div className="flex-1 p-6">
+      {/* Main content area - currently blank as requested */}
+    </div>
+  );
+}
