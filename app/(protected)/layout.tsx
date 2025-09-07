2e6777f1ea3e1280b7e309c157a8b26ad24d5f9f
transatlanticvoyage <sandbagexpert@gmail.com>
2025-09-07 13:23:53 -0400

    dfs

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -4,6 +4,7 @@ import ProtectedRoute from '../components/ProtectedRoute';
 import { LayoutSystemProvider } from '../components/layout-systems';
 import SelectedRowStyles from '../components/SelectedRowStyles';
 import '../styles/shenfur_th_cells_db_table_row.css';
+import '../styles/firestorm_fk_data_badge.css';
 
 export default function ProtectedLayout({
   children,
