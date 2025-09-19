555c5b20df59443a94aae6b29d60c00e639a059c
transatlanticvoyage <sandbagexpert@gmail.com>
2025-09-19 09:39:32 -0400

    sdf

--- shenzi-wordpress-plugins/aardvark/includes/class-aardvark-admin.php
+++ shenzi-wordpress-plugins/aardvark/includes/class-aardvark-admin.php
@@ -7,6 +7,8 @@ class Aardvark_Admin {
     
     public function __construct() {
         add_action('admin_menu', array($this, 'add_admin_menu'));
+        // Hook notice suppression very early for our specific page
+        add_action('current_screen', array($this, 'maybe_suppress_notices'));
     }
     
     public function add_admin_menu() {
@@ -21,10 +23,17 @@ class Aardvark_Admin {
         );
     }
     
+    /**
+     * Check if we're on our page and suppress notices immediately
+     */
+    public function maybe_suppress_notices() {
+        $screen = get_current_screen();
+        if ($screen && $screen->base === 'toplevel_page_papluginsmar') {
+            $this->suppress_all_admin_notices();
+        }
+    }
+    
     public function display_admin_page() {
-        // AGGRESSIVE NOTICE SUPPRESSION - Remove ALL WordPress admin notices
-        $this->suppress_all_admin_notices();
-        
         ?>
         <div class="wrap">
             <h1>Aardvark Plugins Mar</h1>
@@ -37,30 +46,39 @@ class Aardvark_Admin {
      * Based on proven Snefuruplin/Grove implementation
      */
     private function suppress_all_admin_notices() {
-        // Remove all admin notices immediately
-        add_action('admin_print_styles', function() {
-            // Remove all notice actions
-            remove_all_actions('admin_notices');
-            remove_all_actions('all_admin_notices');
-            remove_all_actions('network_admin_notices');
-            
-            // Remove user admin notices
-            global $wp_filter;
-            if (isset($wp_filter['user_admin_notices'])) {
-                unset($wp_filter['user_admin_notices']);
-            }
-        }, 0);
+        // Remove notices immediately - don't wait for hooks
+        remove_all_actions('admin_notices');
+        remove_all_actions('all_admin_notices');
+        remove_all_actions('network_admin_notices');
         
-        // Additional cleanup for persistent notices
+        // Remove user admin notices
+        global $wp_filter;
+        if (isset($wp_filter['user_admin_notices'])) {
+            unset($wp_filter['user_admin_notices']);
+        }
+        
+        // Add immediate CSS suppression
         add_action('admin_head', function() {
-            // Hide any notices that slip through via CSS
             echo '<style type="text/css">
                 .notice, .notice-warning, .notice-error, .notice-success, .notice-info,
                 .updated, .error, .update-nag, .admin-notice,
-                .wrap > .notice, .wrap > .error, .wrap > .updated {
+                .wrap > .notice, .wrap > .error, .wrap > .updated,
+                div[class*="notice"], div[class*="updated"], div[class*="error"] {
                     display: none !important;
                 }
             </style>';
-        });
+        }, 1);
+        
+        // Additional hook-based removal
+        add_action('admin_print_styles', function() {
+            remove_all_actions('admin_notices');
+            remove_all_actions('all_admin_notices');
+            remove_all_actions('network_admin_notices');
+        }, 0);
+        
+        // Nuclear option - remove on admin_notices hook itself
+        add_action('admin_notices', function() {
+            remove_all_actions('admin_notices');
+        }, -9999);
     }
 }
