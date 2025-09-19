a18bc88af7000c991a7caa77015d5def8ea6890c
transatlanticvoyage <sandbagexpert@gmail.com>
2025-09-19 09:25:36 -0400

    dfs

--- shenzi-wordpress-plugins/aardvark/includes/class-aardvark-admin.php
+++ shenzi-wordpress-plugins/aardvark/includes/class-aardvark-admin.php
@@ -22,10 +22,45 @@ class Aardvark_Admin {
     }
     
     public function display_admin_page() {
+        // AGGRESSIVE NOTICE SUPPRESSION - Remove ALL WordPress admin notices
+        $this->suppress_all_admin_notices();
+        
         ?>
         <div class="wrap">
             <h1>Aardvark Plugins Mar</h1>
         </div>
         <?php
+    }
+    
+    /**
+     * AGGRESSIVE NOTICE SUPPRESSION - Remove ALL WordPress admin notices
+     * Based on proven Snefuruplin/Grove implementation
+     */
+    private function suppress_all_admin_notices() {
+        // Remove all admin notices immediately
+        add_action('admin_print_styles', function() {
+            // Remove all notice actions
+            remove_all_actions('admin_notices');
+            remove_all_actions('all_admin_notices');
+            remove_all_actions('network_admin_notices');
+            
+            // Remove user admin notices
+            global $wp_filter;
+            if (isset($wp_filter['user_admin_notices'])) {
+                unset($wp_filter['user_admin_notices']);
+            }
+        }, 0);
+        
+        // Additional cleanup for persistent notices
+        add_action('admin_head', function() {
+            // Hide any notices that slip through via CSS
+            echo '<style type="text/css">
+                .notice, .notice-warning, .notice-error, .notice-success, .notice-info,
+                .updated, .error, .update-nag, .admin-notice,
+                .wrap > .notice, .wrap > .error, .wrap > .updated {
+                    display: none !important;
+                }
+            </style>';
+        });
     }
 }
