295287f12489ff9b0ed40c6211de147c7bb7e76a
transatlanticvoyage <sandbagexpert@gmail.com>
2025-09-19 20:20:39 -0400

    fds

--- shenzi-wordpress-plugins/aardvark/aardvark.php
+++ shenzi-wordpress-plugins/aardvark/aardvark.php
@@ -146,7 +146,7 @@ class AardvarkPlugin {
             array(
                 'plugin_slug' => 'ruplin',
                 'plugin_path' => 'ruplin/ruplin.php',
-                'github_url' => 'https://github.com/transatlanticvoyage/snefuruplin-wp-plugin.git',
+                'github_url' => 'https://github.com/transatlanticvoyage/ruplin.git',
                 'branch_name' => 'main'
             ),
             array(
