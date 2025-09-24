5eb4f6eb222923baa3b3ea2ea8e869a447e48f9f
transatlanticvoyage <sandbagexpert@gmail.com>
2025-09-24 15:02:21 -0400

    sdf

---
+++ other-apps/TransitGun/TransitGun/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TransitGun
+//
+//  Created by Kyle Campbell on 9/24/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ other-apps/TransitGun/TransitGunTests/TransitGunTests.swift
@@ -0,0 +1,17 @@
+//
+//  TransitGunTests.swift
+//  TransitGunTests
+//
+//  Created by Kyle Campbell on 9/24/25.
+//
+
+import Testing
+@testable import TransitGun
+
+struct TransitGunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
