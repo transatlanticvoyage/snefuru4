30d38cf55b096b0b049927c85d4fcd2953ec45b0
transatlanticvoyage <sandbagexpert@gmail.com>
2025-09-25 17:21:23 -0400

    dfs

--- shenzi-wordpress-plugins/klyra/includes/class-klyra-admin.php
+++ shenzi-wordpress-plugins/klyra/includes/class-klyra-admin.php
@@ -45,18 +45,104 @@ class Klyra_Admin {
     }
     
     public function suppress_all_admin_notices() {
-        remove_all_actions('admin_notices');
-        remove_all_actions('all_admin_notices');
-        remove_all_actions('network_admin_notices');
+        // Remove all admin notices immediately
+        add_action('admin_print_styles', function() {
+            // Remove all notice actions
+            remove_all_actions('admin_notices');
+            remove_all_actions('all_admin_notices');
+            remove_all_actions('network_admin_notices');
+            
+            // Remove user admin notices
+            global $wp_filter;
+            if (isset($wp_filter['user_admin_notices'])) {
+                unset($wp_filter['user_admin_notices']);
+            }
+        }, 0);
         
+        // Additional cleanup for persistent notices
         add_action('admin_head', function() {
+            // Hide any notices that slip through via CSS
             echo '<style type="text/css">
                 .notice, .notice-warning, .notice-error, .notice-success, .notice-info,
                 .updated, .error, .update-nag, .admin-notice,
-                .wrap > .notice, .wrap > .error, .wrap > .updated {
+                div.notice, div.updated, div.error, div.update-nag,
+                .wrap > .notice, .wrap > .updated, .wrap > .error,
+                #adminmenu + .notice, #adminmenu + .updated, #adminmenu + .error,
+                .update-php, .php-update-nag,
+                .plugin-update-tr, .theme-update-message,
+                .update-message, .updating-message,
+                #update-nag, #deprecation-warning {
                     display: none !important;
                 }
+                
+                /* Hide WordPress core update notices */
+                .update-core-php, .notice-alt {
+                    display: none !important;
+                }
+                
+                /* Hide plugin activation/deactivation notices */
+                .activated, .deactivated {
+                    display: none !important;
+                }
+                
+                /* Hide file permission and other system warnings */
+                .notice-warning, .notice-error {
+                    display: none !important;
+                }
+                
+                /* Hide any remaining notices in common locations */
+                .wrap .notice:first-child,
+                .wrap > div.notice,
+                .wrap > div.updated,
+                .wrap > div.error {
+                    display: none !important;
+                }
+                
+                /* Nuclear option - hide anything that looks like a notice */
+                [class*="notice"], [class*="updated"], [class*="error"],
+                [id*="notice"], [id*="message"] {
+                    display: none !important;
+                }
+                
+                /* Restore our legitimate content */
+                .klyra-content, .klyra-content * {
+                    display: block !important;
+                }
             </style>';
+        });
+        
+        // Nuclear option - JavaScript cleanup for dynamic notices
+        add_action('admin_footer', function() {
+            echo '<script type="text/javascript">
+                (function($) {
+                    $(document).ready(function() {
+                        // Remove any notices that appear after page load
+                        $(".notice, .updated, .error, .update-nag, .admin-notice").remove();
+                        
+                        // Set up mutation observer to catch dynamically added notices
+                        if (window.MutationObserver) {
+                            var observer = new MutationObserver(function(mutations) {
+                                mutations.forEach(function(mutation) {
+                                    if (mutation.addedNodes.length > 0) {
+                                        $(mutation.addedNodes).find(".notice, .updated, .error, .update-nag").remove();
+                                        $(mutation.addedNodes).filter(".notice, .updated, .error, .update-nag").remove();
+                                    }
+                                });
+                            });
+                            
+                            observer.observe(document.body, {
+                                childList: true,
+                                subtree: true
+                            });
+                        }
+                        
+                        // Periodic cleanup every 500ms
+                        setInterval(function() {
+                            $(".notice, .updated, .error, .update-nag, .admin-notice").not(".klyra-content, .klyra-content *").remove();
+                        }, 500);
+                    });
+                })(jQuery);
+            </script>';
         });
     }
 }
